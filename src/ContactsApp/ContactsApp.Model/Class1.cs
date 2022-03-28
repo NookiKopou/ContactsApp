126e44904fa912a464aacf569f976e49c4adb694
nooki.0103@gmail.com <nooki.0103@gmail.com>
2022-03-28 09:38:37 +0700

    изменение неверного создания библиотеки 2

--- src/ContactsApp/ContactsApp.Model/Class1.cs
+++ src/ContactsApp/ContactsApp.Model/Class1.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ContactsApp.Model
 {
-    class ProjectManager
+    public class Class1
     {
     }
 }
